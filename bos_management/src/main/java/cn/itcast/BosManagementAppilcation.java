d371eb041991daf66b76a0790a027e19ba8fa922
xiong <xiong4201@163.com>
2019-01-18 13:43:26 +0800

    first commit

---
+++ bos_management/src/main/java/cn/itcast/BosManagementAppilcation.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BosManagementAppilcation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BosManagementAppilcation.class,args);
+    }
+
+}
